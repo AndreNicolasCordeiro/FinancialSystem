8a2897fd59d63830afb37474bc708d5143e52a7b
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2022-08-03 15:32:39 -0300

    Att

---
+++ src/components/InputArea/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #FFF;
+    box-shadow: 0px 0px 5px #CCC;
+    border-radius: 10px;
+    padding: 20px;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+`;
+export const InputLabel = styled.label`
+    flex: 1;
+    margin: 10px;
+`;
+export const InputTitle = styled.div`
+    font-weight: bold;
+    margin-bottom: 5px;
+`;
+export const Input = styled.input`
+    width: 100%;
+    height: 30px;
+    padding: 0 5px;
+    border: 1px solid #20212C;
+    border-radius: 5px;
+`;
+export const Select = styled.select`
+    width: 100%;
+    height: 30px;
+    padding: 0 5px;
+    border: 1px solid #20212C;
+    border-radius: 5px;
+`;
+export const Button = styled.button`
+    width: 100%;
+    height: 30px;
+    padding: 0 5px;
+    border: 1px solid #20212C;
+    border-radius: 5px;
+    background-color: #20212C;
+    color: white;
+    cursor: pointer;
+    &:hover {
+        border: 1px solid #20212C;
+        background-color: #3be996;
+        color: #20212C;
+    }
+`;
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 7, 17), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 7, 17), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2022, 7, 17), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2022, 7, 17), category: 'salary', title: 'Salário Trabalho', value: 5000 }
+];
